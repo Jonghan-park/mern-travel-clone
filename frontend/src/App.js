54663af177b4f20d42d485d53e635aa4b55b6056
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 16:10:03 -0600

    Update difference of color depends on user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ import { format } from "timeago.js";
 import "./app.css";
 
 function App() {
+  const currentUser = "Jane";
   const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [viewport, setViewport] = useState({});
   const [showPopup, setShowPopup] = useState(true);
 
@@ -22,49 +24,65 @@ function App() {
     getPins();
   }, []);
 
+  const handleMarkerClick = (id) => {
+    console.log(currentPlaceId);
+    setCurrentPlaceId(id);
+  };
+
   return (
     <div className="App">
       <Map
         initialViewState={{
-          longitude: 136,
-          latitude: 35,
-          zoom: 8,
+          longitude: 119.1565309,
+          latitude: 35.5127629,
+          zoom: 10,
         }}
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "80vw", height: "80vh" }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         mapStyle="mapbox://styles/mapbox/streets-v8"
       >
         {pins.map((p) => (
           <>
             <Marker longitude={p.long} latitude={p.lat} anchor="top">
-              <FaMapMarkerAlt className="pin_icon" />
+              <FaMapMarkerAlt
+                className="pin_icon"
+                style={{
+                  color: p.username === currentUser ? "tomato" : "slateblue",
+                }}
+                onClick={() => handleMarkerClick(p._id)}
+              />
             </Marker>
-            <Popup
-              longitude={p.long}
-              latitude={p.lat}
-              anchor="bottom"
-              onClose={() => setShowPopup(false)}
-            >
-              <div className="card">
-                <label>Place</label>
-                <h4 className="place">{p.title}</h4>
-                <label>Review</label>
-                <p className="desc">{p.desc}</p>
-                <label>Rating</label>
-                <div className="stars">
-                  <FaStar className="star" />
-                  <FaStar className="star" />
-                  <FaStar className="star" />
-                  <FaStar className="star" />
-                  <FaStar className="star" />
+            {p._id === currentPlaceId && (
+              <Popup
+                key={p._id}
+                longitude={p.long}
+                latitude={p.lat}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="bottom"
+                onClose={() => setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
                 </div>
-                <label>Information</label>
-                <span className="username">
-                  Created by <b>{p.username}</b>
-                </span>
-                <span className="date">{format(p.createdAt)}</span>
-              </div>
-            </Popup>
+              </Popup>
+            )}
           </>
         ))}
       </Map>
