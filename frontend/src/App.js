6c1aa67f5ae0fc2bcb1f864a8d608eccb1e1eb49
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 10:16:23 -0600

    Update map and icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <ReactMapGL
+        initialViewState={{
+          longitude: 136,
+          latitude: 35,
+          zoom: 10,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={136} latitude={35} anchor="bottom">
+          <FaMapMarkerAlt />
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
 }
 
 export default App;
