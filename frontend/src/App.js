a6eeb7b01606e978340937efd93649155a5bae06
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 10:43:54 -0600

    Update security

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import Map, { Marker } from "react-map-gl";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <ReactMapGL
+      <Map
         initialViewState={{
           longitude: 136,
           latitude: 35,
-          zoom: 10,
+          zoom: 4,
         }}
         style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        <Marker longitude={136} latitude={35} anchor="bottom">
-          <FaMapMarkerAlt />
+        <Marker longitude={136} latitude={35} anchor="center">
+          {/* <FaMapMarkerAlt /> */}
+          <h1>Hello</h1>
         </Marker>
-      </ReactMapGL>
+      </Map>
     </div>
   );
 }
