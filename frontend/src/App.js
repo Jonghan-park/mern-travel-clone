f1ed29caf4a89c9f302a488ec35a52ff3e0c709e
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 14:37:15 -0600

    Update popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
 import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
+import "./app.css";
 
 function App() {
   const [showPopup, setShowPopup] = useState(true);
@@ -12,12 +13,12 @@ function App() {
           latitude: 35,
           zoom: 8,
         }}
-        style={{ width: "80vw", height: "80vh" }}
+        style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/mapbox/streets-v8"
       >
         <Marker longitude={127.024612} latitude={37.5326} anchor="top">
-          <FaMapMarkerAlt style={{}} />
+          <FaMapMarkerAlt className="pin_icon" />
         </Marker>
         {showPopup && (
           <Popup
@@ -30,14 +31,14 @@ function App() {
               <label>Place</label>
               <h4 className="place">Han river</h4>
               <label>Review</label>
-              <p>Beautiful place. I liked it. </p>
+              <p className="desc">Beautiful place. I liked it. </p>
               <label>Rating</label>
               <div className="stars">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
+                <FaStar className="star" />
+                <FaStar className="star" />
+                <FaStar className="star" />
+                <FaStar className="star" />
+                <FaStar className="star" />
               </div>
               <label>Information</label>
               <span className="username">
