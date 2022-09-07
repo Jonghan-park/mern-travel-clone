b27c8bc243c4adb50b3c6cf7dd30c20bc959d074
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 08:27:42 -0600

    Update pins router

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 5000;
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
@@ -12,6 +15,8 @@ mongoose
     console.log("MongoDB connected");
   })
   .catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
 
 app.listen(port, () => {
   console.log("Server is running on 5000");
