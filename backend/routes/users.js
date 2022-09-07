54d23940ec4b9d4027ab2753e5ee3672b103a1a5
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-07 08:48:01 -0600

    Update login function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -25,5 +25,23 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    // Find user
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong username or password");
+
+    // Validate password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong username or password");
+    // Send response
+    res.status(200).json({ _id: user._id, username: user.username });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
