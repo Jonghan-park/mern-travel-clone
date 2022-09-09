150666220b1bba382973543368300a139ccdb7b7
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-08 20:22:39 -0600

    Update register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./register.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import axios from "axios";
 
 const Register = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    try {
+      const res = await axios.post("/users/register", newUser);
+      setError(false);
+      setSuccess(true);
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className="registerContainer">
       <div className="logo">
         <FaMapMarkerAlt /> JosephPin
       </div>
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" ref={nameRef} />
+        <input type="email" placeholder="email" ref={emailRef} />
+        <input type="password" placeholder="password" ref={passwordRef} />
         <button className="registerBtn">Register</button>
-        <span className="success">Successful. You can login now!</span>
-        <span className="failure">Something went wrong!</span>
+        {success && (
+          <span className="success">Successful. You can login now!</span>
+        )}
+        {error && <span className="failure">Something went wrong!</span>}
       </form>
     </div>
   );
