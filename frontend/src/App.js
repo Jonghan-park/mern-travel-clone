939b01216daa1736a34864694de0bf9a192504ec
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-08 20:00:05 -0600

    Update login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
-import Map, { Marker, Popup } from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
 import axios from "axios";
 import { format } from "timeago.js";
 import "./app.css";
 
 function App() {
-  const currentUser = "Jane";
+  const [currentUser, setCurrentUser] = useState(null);
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(1);
   const [viewport, setViewport] = useState({});
   const [showPopup, setShowPopup] = useState(true);
 
@@ -25,32 +28,58 @@ function App() {
     getPins();
   }, []);
 
-  const handleMarkerClick = (id) => {
+  const handleMarkerClick = (id, lat, long) => {
     setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
   };
   const handleAddClick = (e) => {
     console.log(e);
     const lat = e.lngLat.lat;
-    const lng = e.lngLat.lng;
+    const long = e.lngLat.lng;
 
     setNewPlace({
       lat,
-      lng,
+      long,
     });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setTitle(null);
+      setDesc(null);
+      setRating(1);
+      setNewPlace(null);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="App">
-      <Map
+      <ReactMapGL
         initialViewState={{
           longitude: 126.97696994010124,
           latitude: 37.56762617355592,
           zoom: 11,
         }}
-        style={{ width: "80vw", height: "80vh" }}
+        style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        mapStyle="mapbox://styles/mapbox/streets-v8"
+        mapStyle="mapbox://styles/pjh834/cl7se8bx7004715nporb6lacw"
         onDblClick={handleAddClick}
+        transitionDuration="200"
       >
         {pins.map((p) => (
           <>
@@ -60,7 +89,7 @@ function App() {
                 style={{
                   color: p.username === currentUser ? "tomato" : "slateblue",
                 }}
-                onClick={() => handleMarkerClick(p._id)}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
               />
             </Marker>
             {p._id === currentPlaceId && (
@@ -80,11 +109,7 @@ function App() {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <FaStar className="star" />
-                    <FaStar className="star" />
-                    <FaStar className="star" />
-                    <FaStar className="star" />
-                    <FaStar className="star" />
+                    {Array(p.rating).fill(<FaStar className="star" />)}
                   </div>
                   <label>Information</label>
                   <span className="username">
@@ -98,17 +123,49 @@ function App() {
         ))}
         {newPlace && (
           <Popup
-            longitude={newPlace.lng}
+            longitude={newPlace.long}
             latitude={newPlace.lat}
             closeButton={true}
             closeOnClick={false}
             anchor="bottom"
-            onClose={() => setCurrentPlaceId(null)}
+            onClose={() => setNewPlace(null)}
           >
-            HEllo
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Review</label>
+                <textarea
+                  placeholder="Say us something about this place."
+                  onChange={(e) => setDesc(e.target.value)}
+                ></textarea>
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton" type="submit">
+                  Add Pin
+                </button>
+              </form>
+            </div>
           </Popup>
         )}
-      </Map>
+        {currentUser ? (
+          <button className="button logout">Log out</button>
+        ) : (
+          <div className="buttons">
+            <button className="button login">Login</button>
+            <button className="button register">Register</button>
+          </div>
+        )}
+      </ReactMapGL>
     </div>
   );
 }
